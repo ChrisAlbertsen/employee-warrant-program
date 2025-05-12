7c2f7aab5c983e03101659fe81a2fc0037218e33
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2025-05-12 21:55:55 +0200

    Adds nav items and simple employee function

---
+++ Shared/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Shared
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int NumberOfWarrants { get; set; }
+        public decimal Ownership { get; set; }
+    }
+}
